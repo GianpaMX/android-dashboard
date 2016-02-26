c8a64fff9f60d5ad12720a15a27d5ff995d0809f
Juan Villa <juan64@gmail.com>
2016-02-26 11:01:22 -0600

    WIP

--- app/src/test/java/mx/softux/dashboard/MainActivityTest.java
+++ app/src/test/java/mx/softux/dashboard/MainActivityTest.java
@@ -2,22 +2,38 @@ package mx.softux.dashboard;
 
 import android.view.View;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.android.api.Assertions.assertThat;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MainActivityTest {
+
+    private MainActivity mainActivity;
+
+    @Before
+    public void setup() {
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+    }
+
     @Test
     public void testAddTaskButtonExists() {
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
-        View addTask = activity.findViewById(R.id.add_task_button);
+        View addTaskButton = mainActivity.findViewById(R.id.add_task_button);
+        assertThat(addTaskButton).isNotNull();
+    }
 
-        assertThat(addTask).isNotNull();
+    @Test
+    public void testClickingAddTaskButtonShowsBottomSheet() {
+        View addTaskButton = mainActivity.findViewById(R.id.add_task_button);
+        addTaskButton.performClick();
+
+        View bottomSheetView = mainActivity.findViewById(R.id.bottom_sheet_view);
+        assertThat(bottomSheetView).isVisible();
     }
 }
